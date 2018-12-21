64ff89852f073a62f2e63c931d552693f28711c0
Hansungsil <han_148@naver.com>
2018-12-21 17:46:07 +0900

    Added DB Config

--- mysql-db/mysqlDB.js
+++ mysql-db/mysqlDB.js
@@ -1,11 +1,11 @@
 // mysql 구성 추가
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
     port: 3306,
-    user: 'root',
-    password: '111111',
-    database: 'vueboard'
+    user: 'b08f3651de74b4',
+    password: 'fa765e53',
+    database: 'heroku_b5a2bf6f45ca25f'
 });
 
 module.exports = connection;
